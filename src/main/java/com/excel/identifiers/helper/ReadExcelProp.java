072f98d823d5eb6df0d176e002a6c2fc7f93f50f
Mohan Medarametla -X (momedara - SRS BUSINESS SOLUTIONS INDIA PVT LTD at Cisco) <momedara@cisco.com>
2020-10-15 09:54:19 -0700

    added comments

--- src/main/java/com/excel/identifiers/helper/ReadExcelProp.java
+++ src/main/java/com/excel/identifiers/helper/ReadExcelProp.java
@@ -13,8 +13,10 @@ public class ReadExcelProp {
 
 	public void initiate(String propertyFile) {
 		try{
+			//when u want to use in diffrent application as jar(Nullpointer exception)
 			InputStream input =ReadExcelProp.class.getResourceAsStream("/resources/" +propertyFile);
 			if (input == null) {
+				//to use inside eclipse
 				input=ReadExcelProp.class.getClassLoader().getResourceAsStream(propertyFile) ;
 			}
 			prop.load(input);
